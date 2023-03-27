8ad728625baf71923e286bf36edc804d39ac1039
parker <prwilliams5@gmail.com>
2023-03-27 07:50:35 -0500

    refactored, added error handling

--- powershell/office_install.ps1
+++ powershell/office_install.ps1
@@ -1,15 +1,42 @@
-# Set variables
-$sourcePath = "\\server\share\Office2019"
-$installPath = "C:\Program Files\Microsoft Office"
-$productKey = "AAAAA-BBBBB-CCCCC-DDDDD-EEEEE"
-$productId = "ProPlus2019Volume"
-$channel = "PerpetualVL2019"
-$languageId = "en-us"
+$OfficeInstallerUrl = "https://URL-to-Office-Installer"
+$DownloadPath = "$env:TEMP\OfficeInstaller.exe"
+$LicenseKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX" # Replace with a valid license key
+$Arguments = "/configure `"$env:TEMP\OfficeConfiguration.xml`""
 
-# Create Office configuration file
-$configuration = New-OfficeConfiguration -ProductID $productId -LanguageID $languageId -Channel $channel \
-    -Path "$sourcePath\configuration.xml" -ProductKey $productKey
+# Download Office installer
+Try {
+    Invoke-WebRequest -Uri $OfficeInstallerUrl -OutFile $DownloadPath
+} Catch {
+    Write-Host "Error downloading Office installer: $_"
+    Exit 1
+}
+
+# Create Office configuration XML
+$ConfigurationXml = @"
+<Configuration>
+  <Add OfficeClientEdition="64" Channel="Monthly">
+    <Product ID="O365ProPlusRetail">
+      <Language ID="en-us" />
+    </Product>
+  </Add>
+  <Display Level="None" AcceptEULA="TRUE" />
+  <Property Name="AUTOACTIVATE" Value="1" />
+  <Property Name="PIDKEY" Value="$LicenseKey" />
+</Configuration>
+"@
+
+Set-Content -Path "$env:TEMP\OfficeConfiguration.xml" -Value $ConfigurationXml
 
 # Install Office
-Start-OfficeInstallation -ConfigurationPath $configuration.Path -Path "$sourcePath\setup.exe" \
-    -LogPath "$installPath\Logs"\ -InstallPath $installPath -Wait
+Try {
+    Start-Process -FilePath $DownloadPath -ArgumentList $Arguments -Wait -NoNewWindow
+} Catch {
+    Write-Host "Error during Office installation: $_"
+    Exit 1
+}
+
+# Remove installation files
+Remove-Item $DownloadPath
+Remove-Item "$env:TEMP\OfficeConfiguration.xml"
+
+Write-Host "Office installation completed successfully"
